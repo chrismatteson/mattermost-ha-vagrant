d63847a15d1e8706288384e0703beecb6ec9e3a8
Chris Matteson <chris.matteson@gmail.com>
2017-05-13 07:19:51 -0700

    added rhel6 script

--- scripts/rhel6.sh
+++ scripts/rhel6.sh
@@ -8,7 +8,7 @@ sudo yum upgrade -y
 wget http://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 sudo yum localinstall mysql57-community-release-el7-9.noarch.rpm -y
 sudo yum install mysql-community-server -y
-sudo systemctl start mysqld.service
+sudo service mysqld start
 mysql -u root -p"`sudo grep 'temporary password' /var/log/mysqld.log | grep -oE '[^ ]+$'`" --connect-expired-password <<MYSQL_INPUT
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'Password42!';
 create user 'mmuser'@'%' identified by 'Password42!';
@@ -26,39 +26,25 @@ sudo useradd --system --user-group mattermost
 sudo chown -R mattermost:mattermost /opt/mattermost
 sudo chmod -R g+w /opt/mattermost
 sudo sed -i '/"DataSource":/c\        "DataSource": "mmuser:Password42!@tcp(rhel-7-mm-ha-1:3306)/mattermost?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s",' /opt/mattermost/config/config.json
-#sudo sed -i '/"DataSourceReplicas":/c\        "DataSourceReplicas": ["mmuser:Password42!@tcp(rhel-7-mm-ha-2:3306)/mattermost?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s"],' /opt/mattermost/config/config.json
+sudo sed -i '/"DataSourceReplicas":/c\        "DataSourceReplicas": ["mmuser:Password42!@tcp(rhel-7-mm-ha-2:3306)/mattermost?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s"],' /opt/mattermost/config/config.json
 sudo sed -i '/"SqlSettings"/{n;s/postgres/mysql/g}' /opt/mattermost/config/config.json
-sudo sed -i '/"ClusterSettings"/{n;s/false/true/g}' /opt/mattermost/config/config.json
+#sudo sed -i '/"ClusterSettings"/{n;s/false/true/g}' /opt/mattermost/config/config.json
 sudo sed -i '/"InterNodeUrls"/c\        "InterNodeUrls": ["http://rhel-7-mm-ha-1","http://rhel-7-mm-ha-2"]' /opt/mattermost/config/config.json
-sudo cat <<EOF > /etc/systemd/system/mattermost.service
-[Unit]
-Description=Mattermost
-After=syslog.target network.target postgresql-9.4.service
-
-[Service]
-Type=simple
-WorkingDirectory=/opt/mattermost/bin
-User=mattermost
-ExecStart=/opt/mattermost/bin/platform
-PIDFile=/var/spool/mattermost/pid/master.pid
-LimitNOFILE=49152
-
-[Install]
-WantedBy=multi-user.target
+sudo cat <<EOF > /etc/init/mattermost.conf
+start on runlevel [2345]
+stop on runlevel [016]
+respawn
+limit nofile 50000 50000
+chdir /opt/mattermost
+exec bin/platform
 EOF
-sudo chmod 755 /etc/systemd/system/mattermost.service
-sudo systemctl daemon-reload
 sudo chkconfig mattermost on
-sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=8065/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=8075/tcp --permanent
-sudo systemctl restart firewalld
-sudo systemctl enable mattermost
-sudo systemctl start mattermost
-
-# Install license
 sudo /opt/mattermost/bin/platform license upload /vagrant/license.mattermost-license
-sudo systemctl restart mattermost
+sudo iptables -A INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp --dport 8065 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp --dport 8075 -j ACCEPT
+iptables-save | sudo tee /etc/sysconfig/iptables
+sudo start mattermost
 
 #Install NGINX
 sudo cat <<EOF > /etc/yum.repos.d/nginx.repo
@@ -69,8 +55,8 @@ gpgcheck=0
 enabled=1
 EOF
 sudo yum install nginx.x86_64 -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo service nginx start
+sudo chkconfig nginx on
 
 #Configure NGINX
 #sudo cat <<EOF > /etc/nginx/sites-available/mattermost
@@ -124,7 +110,7 @@ EOF
 #sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/conf.d/default.conf
 #sudo ln -s /etc/nginx/sites-available/mattermost /etc/nginx/sites-enabled/mattermost
-sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
-sudo systemctl restart firewalld
-sudo systemctl restart nginx
+sudo iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables-save | sudo tee /etc/sysconfig/iptables
+sudo service nginx restart
 
